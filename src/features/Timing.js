9a405bf10f5ff4ff7cd8cce0dde89328bd39ac22
Saul Flores <saulpolo95@gmail.com>
2022-12-18 19:06:44 -0800

    added base projedct

---
+++ src/features/Timing.js
@@ -0,0 +1,25 @@
+import { View, StyleSheet } from "react-native";
+import { RoundedButton } from "../components/RoundedButton";
+const Timing = ({ onChangeTime }) => {
+  return (
+    <>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="10" onPress={() => onChangeTime(10)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="20" onPress={() => onChangeTime(20)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="30" onPress={() => onChangeTime(30)} />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  timingButton: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
+export default Timing;
